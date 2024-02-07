78227b483bda0b3660ba4af6812200e8c0d37509
rk28284 <112754760+rk28284@users.noreply.github.com>
2024-02-07 10:45:09 +0530

    Headers Created

--- netflixgpt/src/App.js
+++ netflixgpt/src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { Bodyflix } from "./Components/Bodyflix";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+    
+<Bodyflix/>
     </div>
   );
 }
---
+++ netflixgpt/src/Components/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Hearder } from './Hearder'
+
+export const Login = () => {
+  return (
+    <div>
+        <Hearder/>
+        <div>
+            <img 
+            srcset="https://assets.nflxext.com/ffe/siteui/vlv3/4da5d2b1-1b22-498d-90c0-4d86701dffcc/98a1cb1e-5a1d-4b98-a46f-995272b632dd/IN-en-20240129-popsignuptwoweeks-perspective_alpha_website_small.jpg 1000w, https://assets.nflxext.com/ffe/siteui/vlv3/4da5d2b1-1b22-498d-90c0-4d86701dffcc/98a1cb1e-5a1d-4b98-a46f-995272b632dd/IN-en-20240129-popsignuptwoweeks-perspective_alpha_website_medium.jpg 1500w, https://assets.nflxext.com/ffe/siteui/vlv3/4da5d2b1-1b22-498d-90c0-4d86701dffcc/98a1cb1e-5a1d-4b98-a46f-995272b632dd/IN-en-20240129-popsignuptwoweeks-perspective_alpha_website_large.jpg 1800w"
+            alt="banner" />
+        </div>
+    </div>
+  )
+}
