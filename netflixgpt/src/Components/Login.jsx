d7188c1347c600c5e4ec2bff6620e033cf188ee4
rk28284 <112754760+rk28284@users.noreply.github.com>
2024-02-24 20:42:33 +0530

    Form validation addded

--- netflixgpt/src/Components/Login.jsx
+++ netflixgpt/src/Components/Login.jsx
@@ -1,8 +1,21 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Hearder } from './Hearder'
+import { checkValiadater } from '../Utilities/valiadater'
 
 export const Login = () => {
   const [isSignIn,setIsSignIn]=useState(true)
+  const [isError,setIsError]=useState(null)
+
+const email=useRef(null)
+const password=useRef(null)
+const handleButtonclick=()=>{
+  //validate the form
+const message= checkValiadater(email.current.value,password.current.value)
+
+  console.log(message);
+setIsError(message)
+}
+
 
   const toggleForm=()=>{
     setIsSignIn(!isSignIn)
@@ -15,18 +28,19 @@ export const Login = () => {
             srcset="https://assets.nflxext.com/ffe/siteui/vlv3/4da5d2b1-1b22-498d-90c0-4d86701dffcc/98a1cb1e-5a1d-4b98-a46f-995272b632dd/IN-en-20240129-popsignuptwoweeks-perspective_alpha_website_small.jpg 1000w, https://assets.nflxext.com/ffe/siteui/vlv3/4da5d2b1-1b22-498d-90c0-4d86701dffcc/98a1cb1e-5a1d-4b98-a46f-995272b632dd/IN-en-20240129-popsignuptwoweeks-perspective_alpha_website_medium.jpg 1500w, https://assets.nflxext.com/ffe/siteui/vlv3/4da5d2b1-1b22-498d-90c0-4d86701dffcc/98a1cb1e-5a1d-4b98-a46f-995272b632dd/IN-en-20240129-popsignuptwoweeks-perspective_alpha_website_large.jpg 1800w"
             alt="banner" />
         </div>
-        <form className='w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0
+        <form onSubmit={(e)=>(e.preventDefault())} className='w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0
          text-white rounded-lg bg-opacity-80'>
          <h1 className='font-bold text-3xl'>{isSignIn?"Sign In":"Sign Out"}</h1>
 {!isSignIn&&<input type="text"
            placeholder="Enter Your Full Name" className='p-4 my-4 w-full bg-gray-700' />}
-          <input type="email"
+          <input type="email" ref={email}
            placeholder="Enter Your Email" className='p-4 my-4 w-full bg-gray-700' />
         
-          <input type="password" placeholder="Enter Your Password" 
+          <input type="password"   ref={password} placeholder="Enter Your Password"
           className='p-4 my-4 w-full bg-gray-700' />
+<p className='text-red-500 font-bold text-lg py-4'>{isError}</p>
 
-          <button className='p-4 my-6 bg-red-700 w-full rounded-lg'>
+          <button onClick={handleButtonclick} className='p-4 my-6 bg-red-700 w-full rounded-lg'>
           {isSignIn?"Sign In":"Sign Out"}
             </button>
 
